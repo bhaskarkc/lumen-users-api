61108a8fb8b287898200343f26cd9ffab0ecf06b
Bhaskar K <xlinkerz@gmail.com>
2020-12-30 07:14:41 +1100

    DB seeder: User table

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        User::truncate();
+        User::factory()
+            ->times(20)
+            ->create();
     }
 }

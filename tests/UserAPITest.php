4b349d12a5fb732b588be7aa0c21c8d95024f7ed
Bhaskar K <xlinkerz@gmail.com>
2020-12-30 09:12:29 +1100

    API tests

--- tests/UserAPITest.php
+++ tests/UserAPITest.php
@@ -6,26 +6,33 @@ class UserAPITest extends TestCase
 {
     use DatabaseTransactions;
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->get('/');
-
-        $this->assertEquals(
-            $this->app->version(),
-            $this->response->getContent()
-        );
-    }
-
     public function testCreateUser()
     {
         $user = ['name' => 'Bhaskar', 'email' => 'xlinkerz@gmail.com'];
         $response = $this->call('POST', '/api/v1/user', $user);
         $this->assertEquals(200, $response->status());
         $this->seeInDatabase('users', ['email' => 'xlinkerz@gmail.com']);
+    }
+
+
+    public function testViewUser()
+    {
+        $response = $this->call('GET', '/api/v1/user/1');
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testUpdateUser()
+    {
+        $updated_user = ['name' => 'Bhaskar', 'email' => 'updated@gmail.com'];
+        $response = $this->call('PUT', '/api/v1/user/1', $updated_user);
+        $this->assertEquals(200, $response->status());
+    }
+
+
+    public function testDeleteUser()
+    {
+        $response = $this->call('DELETE', '/api/v1/user/1');
+        $this->seeJsonEquals(["message" => "User deleted."]);
+        $this->assertEquals(200, $response->status());
     }
 }

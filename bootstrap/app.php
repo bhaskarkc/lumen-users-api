800c705c2e907c0ff46473f8526cfa1b56150a6f
Bhaskar K <xlinkerz@gmail.com>
2020-12-29 12:09:08 +1100

    refac bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
+(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(dirname(__DIR__)))->bootstrap();
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
@@ -19,11 +17,9 @@ date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 |
 */
 
-$app = new Laravel\Lumen\Application(
-    dirname(__DIR__)
-);
+$app = new Laravel\Lumen\Application(dirname(__DIR__));
 
-$app->withFacades();
+/* $app->withFacades(); */
 
 $app->withEloquent();
 
@@ -93,7 +89,7 @@ $app->configure('app');
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -106,9 +102,7 @@ $app->configure('app');
 |
 */
 
-$app->router->group([
-    'namespace' => 'App\Http\Controllers',
-], function ($router) {
+$app->router->group([ 'namespace' => 'App\Http\Controllers' ], function ($router) {
     require __DIR__ . '/../routes/web.php';
 });
 
